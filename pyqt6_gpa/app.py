d3cb200756f27ca110b8e4229109f06439fa3860
Nicolas_RyanJaden <nicor752@hsd.k12.or.us>
2024-05-31 08:19:24 -0700

    Home

--- pyqt6_gpa/app.py
+++ pyqt6_gpa/app.py
@@ -1,6 +1,8 @@
 import sys
 
 from PyQt6.QtCore import Qt
+import controller
+
 from PyQt6.QtWidgets import (
     QApplication,
     QComboBox,
@@ -24,7 +26,6 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("GPA Calculator App")
-        
         self.UIComponents()
 
 # Setup for the title
@@ -42,18 +43,8 @@ class MainWindow(QMainWindow):
         self.title_label.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.main_layout.addWidget(self.title_label, 0, 0)
 
- 
-
-        # self.left_panel = QWidget()
-        # self.left_panel_layout = QVBoxLayout()
-        # self.left_panel.setLayout(self.left_panel_layout)
-        # self.main_layout.addWidget(self.left_panel)
-
-
-
         self.grade_inputs = QWidget
         
-
 # Area where user enters class grades
         class_label = QLabel("Enter your class grade here:")
         h2_font = class_label.font()
@@ -153,24 +144,17 @@ class MainWindow(QMainWindow):
         class_8_container_layout.addWidget(self.enter_grade_class_8, 0 , 1)
         #Title
         calc_desc_container = QGroupBox("How does the GPA calculator work?")
+        description = QLabel("The app requires you to put an input for each of your classes, which is it's current grade. It will calculate the average of all those inputs, and it will display your school's GPA")
         calc_desc_container_layout = QGridLayout()
         calc_desc_container.setLayout(calc_desc_container_layout)
         
-        #Description
-        #calc_text = QLabel("Test")
+#Description
+ 
 # Results area
         results_label = QLabel("Your unweighted GPA is...")
         h2_font = results_label.font()
         h2_font.setPointSize(25)
         results_label.setFont(h2_font)
-
-#def calculate_grade(self):
-        "Calculate GPA"
-# All class grades
-#def calculate_grade(self):
-       # gpa = enter_grade_class_3.value()
-       # print(gpa)
-        #self.results_window.Text("Your GPA is {gpa}.")
 
 # Button calculate
         calculate_label = QLabel("Calculate it!")
@@ -178,6 +162,9 @@ class MainWindow(QMainWindow):
         h2_font.setPointSize(50)
         calculate_label.setFont(h2_font)
         calculate_label.setAlignment(Qt.AlignmentFlag.AlignRight)
+# add calculate function
+        self.push_button = QPushButton("Calculate!")
+        self.push_button.clicked.connect(self.calculate_grade)
 
 # Description of app
         info_label = QLabel("The GPA calculator app allows you to...")
@@ -187,15 +174,8 @@ class MainWindow(QMainWindow):
         info_label.setAlignment(Qt.AlignmentFlag.AlignRight)
         info_label.setAlignment(Qt.AlignmentFlag.AlignTop)
 
-        self.push_button = QPushButton("Calculate!")
-        # add calculate function
-        self.push_button.clicked.connect(self.calculate_grade)
-
 # Display the average
-        self.results_window = QLabel("Average")
-
-            
-            
+        self.results_window = QLabel("Your GPA is....")
         
 # Calls the function
         self.main_layout.addWidget(class_1_container, 1, 0)
@@ -207,29 +187,48 @@ class MainWindow(QMainWindow):
         self.main_layout.addWidget(class_7_container, 7, 0)
         self.main_layout.addWidget(class_8_container, 8, 0)
         self.main_layout.addWidget(calc_desc_container, 1, 1)
-        
-        #self.main_layout.addWidget(calc_text, 2, 1)
+        self.main_layout.addWidget(description, 1, 1)
         self.main_layout.addWidget(self.push_button, 2, 1)
         self.main_layout.addWidget(self.results_window, 3, 1)
         
-
-
-
-        # right_pane.addWidget(results_label)
-        # left_pane.addWidget(class_label)
-        # right_pane.addWidget(calculate_label)
-        
-
-        # self.main_layout.addLayout(left_pane, 1, 0)
-        # self.main_layout.addLayout(right_pane, 1, 2)
-
     def calculate_grade(self):
         """Calculate gpa"""
-        gpa = self.enter_class_grade_1.value()
-        print(gpa)
-        self.results_window.setText(f"Your GPA is {gpa}.")
+        grade_list = []
+        grade = self.enter_grade_class_1.value()
+        if grade > 0:
+            grade_list.append(grade)
 
+        grade = self.enter_grade_class_2.value()
+        if grade > 0:
+                grade_list.append(grade)
 
+        grade = self.enter_grade_class_3.value()
+        if grade > 0:
+                grade_list.append(grade)
+
+        grade = self.enter_grade_class_4.value()
+        if grade > 0:
+                grade_list.append(grade)
+
+        grade = self.enter_grade_class_5.value()
+        if grade > 0:
+                grade_list.append(grade)
+
+        grade = self.enter_grade_class_6.value()
+        if grade > 0:
+                grade_list.append(grade)
+
+        grade = self.enter_grade_class_7.value()
+        if grade > 0:
+                grade_list.append(grade)
+
+        grade = self.enter_grade_class_8.value()
+        if grade > 0:
+                grade_list.append(grade)
+        
+        results = controller.get_gradegpa(grade_list)
+        print(results)
+        self.results_window.setText(results)
 
 app = QApplication(sys.argv)
 window = MainWindow()
--- pyqt6_gpa/controller.py
+++ pyqt6_gpa/controller.py
@@ -5,10 +5,9 @@ Some functions to control our GPA calculator app
 # imports
 
 # Functions
-def get_gradegpa(grade1: int, grade2: int, grade3: int, grade4: int, grade5: int, grade6: int, grade7: int, grade8: int) -> float:
+def get_gradegpa(grades: list) -> float:
     """Takes averages"""
-    class_amount = 8
-    gradegpa = round(((grade1 + grade2 + grade3 + grade4 + grade5 + grade6 + grade7 + grade8 ) / (class_amount)),2)
+    class_amount = len(grades)
     results = f"Your GPA is... {gradegpa}"
     return results
 # Global scope
